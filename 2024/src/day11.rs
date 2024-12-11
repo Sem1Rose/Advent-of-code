4326ea9d3cfa02d8f994cfe7cd34956dbc9f6424
Sem1Rose <AppdullahMohammed@gmail.com>
2024-12-11 23:59:32 +0200

    finished day 11 part 2
    was able to reduce the depth from 75 to 39, since it only takes like a
    minute to get all the pebbles using the first part's solution. then
    we're discarding the dublicate pebbles, and then hashing the pebble
    count after applying the rest of the 75 passes (36) on each of the
    pebbles. these hashes are then used to count the number of items that we
    would've gotten if we were to use the algorithm with full depth. this
    possibly saves freaking eons of computation time and terabytes of
    dedicated ram, and grants me one extra star this year :).

--- 2024/src/day11.rs
+++ 2024/src/day11.rs
@@ -1,5 +1,6 @@
 use std::{
-    // collections::HashMap,
+    collections::HashMap,
+    collections::HashSet,
     fs::read_to_string,
     sync::{Arc, Mutex},
     thread,
@@ -44,8 +45,21 @@ pub fn part_two() {
         .map(|x| x.parse::<u64>().unwrap())
         .collect::<Vec<_>>();
 
+    let depth = calc_blinks(pebbles, 39);
+    let hashes = HashSet::from_iter(depth.clone());
+
+    println!("{}", hashes.len());
+
+    let hashmap = populate_hashmap(hashes, 36);
+
+    let mut sum = 0;
+    for i in depth {
+        sum += hashmap.get(&i).unwrap();
+    }
+
+    println!("{sum}");
     // println!("{}", pebbles.join(" "));
-    println!("{}", calc_blinks(pebbles, 75));
+    // println!("{}", calc_blinks(pebbles, &mut hashmap, 35, 0).len());
 }
 
 // fn blink2(pebbles: &[u128]) -> Vec<u128> {
@@ -111,18 +125,16 @@ pub fn part_two() {
 //     x
 // }
 
-fn calc_blinks(pebbles: Vec<u64>, num_passes: u32) -> usize {
-    let new_pebbles_count = Arc::new(Mutex::new(0usize));
+fn populate_hashmap(pebbles: HashSet<u64>, depth: u32) -> HashMap<u64, u64> {
+    let hashmap = Arc::new(Mutex::new(HashMap::<u64, u64>::new()));
     let mut handles = vec![];
-    for i in 0..pebbles.len() {
-        let pebble = pebbles[i];
-        let counter = Arc::clone(&new_pebbles_count);
+    for (i, pebble) in pebbles.clone().into_iter().enumerate() {
+        let h_map = Arc::clone(&hashmap);
 
         let handle = thread::spawn(move || {
-            let mut pebbs_tree = vec![pebble];
-            for j in 0..num_passes {
-                println!("{i} => {j}: {}", pebbs_tree.len());
-                pebbs_tree = pebbs_tree
+            let mut tree = vec![pebble];
+            for _ in 0..depth {
+                tree = tree
                     .iter()
                     .flat_map(|x| {
                         if *x == 0 {
@@ -137,23 +149,62 @@ fn calc_blinks(pebbles: Vec<u64>, num_passes: u32) -> usize {
                         }
                     })
                     .collect();
-                // for i in 0..pebbs_tree.len() {
-                //     let pebb = pebbs_tree[i];
-                //     if pebb == 0 {
-                //         new_tree.push(1);
-                //     } else {
-                //         let len = (0..).take_while(|x| 10u64.pow(*x) <= pebb).count();
-                //         if len % 2 == 0 {
-                //             new_tree.push(pebb / 10u64.pow(len as u32 / 2));
-                //             new_tree.push(pebb % 10u64.pow(len as u32 / 2));
-                //         } else {vec
-                //             new_tree.push(pebb * 2024);
-                //         }
-                //     }
-                // }
             }
 
-            *counter.lock().unwrap() += pebbs_tree.len();
+            h_map.lock().unwrap().insert(pebble, tree.len() as u64);
+            println!("{i}, {pebble}");
+            drop(tree);
+        });
+        handles.push(handle);
+
+        while handles.len() >= 16 {
+            thread::sleep(std::time::Duration::from_millis(100));
+            for i in 0..handles.len() {
+                if handles[i].is_finished() {
+                    handles.remove(i);
+                    break;
+                }
+            }
+        }
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    let x = hashmap.lock().unwrap().clone();
+    x
+}
+
+fn calc_blinks(pebbles: Vec<u64>, depth: u32) -> Vec<u64> {
+    let final_tree = Arc::new(Mutex::new(vec![]));
+    let mut handles = vec![];
+    for i in 0..pebbles.len() {
+        let pebble = pebbles[i];
+        let f_tree = Arc::clone(&final_tree);
+
+        let handle = thread::spawn(move || {
+            let mut tree = vec![pebble];
+            for j in 0..depth {
+                println!("{i} => {j}: {}", tree.len());
+                tree = tree
+                    .iter()
+                    .flat_map(|x| {
+                        if *x == 0 {
+                            vec![1]
+                        } else {
+                            let len = (0..).take_while(|y| 10u64.pow(*y) <= *x).count();
+                            if len & 1 == 0 {
+                                vec![x / 10u64.pow(len as u32 / 2), x % 10u64.pow(len as u32 / 2)]
+                            } else {
+                                vec![*x * 2024]
+                            }
+                        }
+                    })
+                    .collect();
+            }
+
+            f_tree.lock().unwrap().extend(tree);
         });
         handles.push(handle);
     }
@@ -161,7 +212,8 @@ fn calc_blinks(pebbles: Vec<u64>, num_passes: u32) -> usize {
     for handle in handles {
         handle.join().unwrap();
     }
-    let x = *new_pebbles_count.lock().unwrap();
+
+    let x = final_tree.lock().unwrap().clone();
     x
 }
 
